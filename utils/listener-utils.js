129eb50b56c41456297064b6db4a73688f516a68
Jeroen Matthijssens <jeroen.matthijssens@bewire.be>
2016-10-06 17:32:52 +0200

    Use logging module.

--- utils/listener-utils.js
+++ utils/listener-utils.js
@@ -1,5 +1,6 @@
 var amqp = require('amqplib');
 var chalk = require ('chalk');
+var Logger = require ('./logger').Logger;
 var q = require ('q');
 
 (function () {
@@ -11,36 +12,18 @@ var q = require ('q');
 		}
 	}
 
-	function printObject (obj) {
-		JSON.stringify (obj, null, '\t').split ('\n').forEach (function (line) {
-			console.log ('    ', line);
-		});
-	}
-
-	function completeOptions (options) {
-		options.broker = options.broker || 'amqp://guest:guest@localhost:5672?heartbeat=120';
-		options.correlationProperties = options.correlationProperties || ['correlation_id', 'pid', 'directory'];
-		options.listenqueue = options.listenqueue || 'requests';
-		options.replyqueue = options.replyqueue || 'replies';
-		options.durable = options.durable || true;
-		options.acknoledge = options.acknowledge || true;
-		options.reconnectTimeout = options.reconnectTimeout || 10000;
-		options.reconnectLimit = options.reconnectLimit || 10;
-
-		return options;
-	}
-
 	var Connection = function Connection (options, fn) {
-		this.options = completeOptions (options);
+		this.options = options;
 		this.fn = fn;
 
 		this.conn = null;
 		this.channel = null;
+		this.logger = new Logger (options);
 
 		this.connect ().catch (function (err) {
-			console.log (chalk.bold (' [!] ') + chalk.red ('Establishing listener failed'));
-			console.log (err);
-		});
+			this.logger.error ('Establishing listener failed', { error: err.message });
+			console.warn (err);
+		}.bind (this));
 	}
 
 	Connection.prototype.connect = function connect () {
@@ -56,15 +39,14 @@ var q = require ('q');
 	Connection.prototype.establishConnection = function establishConnection (count) {
 		count = count || 1;
 		if ( count > this.options.reconnectLimit ) {
-			console.warn (chalk.bold (' [!] ') + chalk.red ('Too many connection attempts. Shutting down.'));
+			this.logger.error ('Too many connection attempts. Shutting down.');
 			return q.reject ('too many connect attempts');
 		}
 
 		return amqp.connect (this.options.broker).catch (function (err) {
-			console.log (chalk.bold (' [C] ') + chalk.yellow ('Connection attempt failed ...'));
-			console.log (chalk.yellow ('     broker url: %s'), this.options.broker);
-			console.log (chalk.yellow ('     Retrying in %s seconds (attempt %s)'), this.options.reconnectTimeout, count);
-			console.log (err);
+			this.logger.warn ('connection attempt failed ...', { broker_url: this.options.broker, err: err.message });
+			this.logger.warn (`retrying in ${this.options.recconectTimeout} miliseconds (attempt ${count})`);
+			console.warn (err);
 
 			var deferred = q.defer ();
 			setTimeout (function () {
@@ -98,13 +80,12 @@ var q = require ('q');
 
 	Connection.prototype.addConnectionListeners = function addConnectionLisneners (conn) {
 		conn.on ('close', function connectionClosed () {
-			console.log (chalk.bold (' [C] ') + chalk.yellow ('Connection closed ...'));
-		});
+			this.logger.warn ('Connection closed');
+		}.bind (this));
 
 		conn.on ('error', function connectionError (err) {
-			console.warn (chalk.bold (' [!] ') + chalk.red ('Connection error:'));
-			console.warn (err);
-			console.warn ('    ' + chalk.red ('reconnecting in ' + this.options.reconnectTimeout + ' miliseconds.'));
+			this.logger.error ('Connection error', { error: err.message });
+			this.logger.error (`reconnecting in ${this.options.reccconectTimeout} miliseconds.`);
 			setTimeout (this.reconnect.bind (this, 1), this.options.reconnectTimeout);
 		}.bind (this));
 
@@ -123,11 +104,11 @@ var q = require ('q');
 	}
 
 	Connection.prototype.startConsuming = function startConsuming (channel) {
-		console.log (
+		this.logger.output (
 			chalk.bold (' [*] ') + chalk.gray ('Waiting for messages (%s -> %s)'),
 			this.options.listenqueue, this.options.replyqueue
 		);
-		console.log (chalk.gray ('     To exit press CTRL+C'));
+		this.logger.output (chalk.gray ('     To exit press CTRL+C'));
 
 		this.channel.consume (this.options.listenqueue, this.consumer.bind (this));
 
@@ -146,23 +127,18 @@ var q = require ('q');
 		try { promise = q (this.fn (this.channel, data, response) || response); }
 		catch (e) { promise = q.reject (e); }
 
-		promise = promise.catch (function (e) {
-			console.warn (chalk.bold (' [!] ') + chalk.red (e));
-			console.warn (e);
+		promise = promise.catch (function (err) {
+			this.logger.error ('Error while processing message', { error: err.message });
+			console.warn (err);
+
 			response.success = false;
+			response.error = err.message;
 			return response;
-		});
+		}.bind (this));
 
 		promise = promise.finally (function (r) {
-			console.log (
-				chalk.bold (" [x] ") + chalk.green ("(from %s) Received request ..."),
-				this.options.listenqueue
-			);
-			if ( this.options.verbose ) {
-				printObject (data)
-				console.log (chalk.green ("     (to %s) Replying with ..."), this.options.replyqueue);
-				printObject (response);
-			};
+			this.logger.log (`(from ${this.options.listenqueue}) Recieved request ...`, data);
+			this.logger.log (`(to ${this.options.replyqueue}) Replying with ...`, response);
 		}.bind (this));
 
 		promise = promise.then (function (res) {
@@ -172,8 +148,9 @@ var q = require ('q');
 		}.bind (this));
 
 		promise.catch (function (err) {
-			console.warn (chalk.red (' [!] %s'), err);
-		});
+			this.logger.error ('Failed to send response', { error: err.message });
+			console.warn (err);
+		}.bind (this));
 	}
 
 	module.exports = {

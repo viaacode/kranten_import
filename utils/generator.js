b525a6aa3fdbbce6752f30d99bcd369de9d7b39b
dietervanhoof <dieter.vanhoof@c4j.be>
2017-02-09 14:06:55 +0100

    Fixed METS type again.

--- utils/generator.js
+++ utils/generator.js
@@ -88,7 +88,7 @@ var calcSimpleType = fUtils.calcSimpleType;
 		var loc = addNode (this.xml, metsnode, ['mets:FLocat']);
 		metsnode.attr ('ID', 'id_' + id + '_mets');
 		metsnode.attr ('MIMETYPE', 'application/xml');
-		metsnode.attr ('USE', this.config[calcSimpleType('filename_mets.xml')]);
+		metsnode.attr ('USE', this.config.fileUse[calcSimpleType('filename_mets.xml')]);
 		loc.attr ('LOCTYPE', 'OTHER');
 		loc.attr ('xlink:href', id + '_mets.xml');
 	}
